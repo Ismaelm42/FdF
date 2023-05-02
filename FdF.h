74dd0f987311d12202f91bc825f5b2111631a98d
Ismael <imoro-sa@student.42malaga.com>
2023-05-02 21:27:57 +0200

    files deleted

--- FdF.h
+++
@@ -1,6 +0,0 @@
-#ifndef FDF_H
-# define FDF_H
-
-# include "libft/libft.h"
-# include "./MLX42/include/MLX42/MLX42.h"
-#endif
--- check_map.c
+++
@@ -1,9 +0,0 @@
-# include "FdF.h"
-
-int	main(void)
-{
-	char	str[] = "HOLA";
-	ft_strlen(str);
-	ft_putchar_fd('a', 1);
-	return (0);
-}
