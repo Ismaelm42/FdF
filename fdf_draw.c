26b63c271dd6f9fe3abeb83899aba3985ebd8bd0
Ismael <imoro-sa@student.42malaga.com>
2023-05-11 21:34:50 +0200

    bresenham ok

--- fdf_draw.c
+++ fdf_draw.c
@@ -40,6 +40,8 @@ t_coord	*coord_init(t_point *a, t_point *b)
 void	bresenham(t_point *a, t_point *b, t_fdf *fdf)
 {
 	t_coord	*coord;
+	int		x;
+	int		y;
 	int		pixel;
 
 	if (a->x == 1 && a->y == 4)
@@ -47,25 +49,26 @@ void	bresenham(t_point *a, t_point *b, t_fdf *fdf)
 		printf("AWsx=1 ==%d\n", fdf->map->points[0][1].sx);
 		printf("AWsy=0 ==%d\n\n\n\n\n\n\n\n", fdf->map->points[0][1].sy);
 	}
-	//tengo que utilizar coordenadas x e y, creo;
 	coord = coord_init(a, b);
+	x = a->sx;
+	y = a->sy;
 	while (1)
 	{
-		pixel = (a->sx * 4) + (a->sy * fdf->mlx_image->width * 4);
-		if (a->sx < fdf->w_width && a->sy < fdf->w_heigth)
+		pixel = (x * 4) + (y * fdf->mlx_image->width * 4);
+		if (x < fdf->w_width && x < fdf->w_heigth)
 			put_pixel(pixel, a, b, fdf);
-		if (a->sx == b->sx && a->sy == b->sy)
+		if (x == b->sx && y == b->sy)
 			break ;
 		coord->err_2 = 2 * coord->err;
 		if (coord->err_2 >= coord->dy)
 		{
 			coord->err = coord->err + coord->dy;
-			a->sx = a->sx + coord->inc_x;
+			x = x + coord->inc_x;
 		}	
 		if (coord->err_2 <= coord->dx)
 		{
 			coord->err = coord->err + coord->dx;
-			a->sy = a->sy + coord->inc_y;
+			y = y + coord->inc_y;
 		}
 	}
 	free(coord);
